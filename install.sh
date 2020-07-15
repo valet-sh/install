982c6d6d03d3d95efa4dd9c99a578c6ad3807146
Johann Zelger <j.zelger@techdivision.com>
2020-07-15 21:15:06 +0200

    Update install mechanism, removed ostype for linux-gnu. fixed macos big sur compatibility

--- install.sh
+++ install.sh
@@ -31,26 +31,23 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Check Command Line Tools"
     # if git command is not available, install CLT
-    if [ ! -f /Library/Developer/CommandLineTools/usr/bin/git ]; then
-        echo "Check Command Line Tools"
+    if [ ! -f /Library/Developer/CommandLineTools/usr/bin/git ]; then    
         # create macOS flag file, that CLT can be installed on demand
-        touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+        sudo touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
         # parse CLT software name
-        SOFTWARE_CLT_NAME=$(
-            softwareupdate -l |
+        SOFTWARE_CLT_NAME=$(bash -c "/usr/sbin/softwareupdate -l | 
             grep -B 1 -E 'Command Line Tools' |
-            awk -F'*' '/^ +\*/ {print $2}' |
-            sed 's/^ *//' |
-            grep -iE '[0-9|.]' |
-            sort |
-            tail -n1
-        )
+            awk -F'*' '/^ *\\*/ {print \$2}' |
+            sed -e 's/^ *Label: //' -e 's/^ *//' |
+            sort -V |
+            tail -n1")
         echo "Installing ${SOFTWARE_CLT_NAME}"
         # install CLT
         softwareupdate -i "${SOFTWARE_CLT_NAME}" &> /dev/null
         # cleanup
-        rm -rf /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+        sudo rm -rf /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
     fi
 
     # check if pip is available
@@ -71,6 +68,11 @@ fi
 # check if valet-sh is installed
 if [ ! -d "${VSH_REPO_DIR}" ]; then
     echo "Installing valet-sh"
+    APPLICATION_OSTYPE=""
+    # define os specific filter only for macos till unirelease is ready
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        APPLICATION_OSTYPE="darwin"
+    fi
     # retrigger sudo
     sudo true
     # cleanup old installations (< version 1.0.0-alpha10)
@@ -88,7 +90,7 @@ if [ ! -d "${VSH_REPO_DIR}" ]; then
     # fetch all tags from application git repo
     git --git-dir="${VSH_REPO_DIR}/.git" --work-tree="${VSH_REPO_DIR}" fetch --tags --quiet
     # get available git tags sorted by refname
-    GIT_TAGS=$(git --git-dir="${VSH_REPO_DIR}/.git" --work-tree="${VSH_REPO_DIR}" tag --sort "-v:refname" | grep "${OSTYPE}")
+    GIT_TAGS=$(git --git-dir="${VSH_REPO_DIR}/.git" --work-tree="${VSH_REPO_DIR}" tag --sort "-v:refname" | grep "${APPLICATION_OSTYPE}")
     # get latest semver conform git version tag
     for GIT_TAG in ${GIT_TAGS}; do
         # validate tag to be semver compliant
@@ -100,7 +102,7 @@ if [ ! -d "${VSH_REPO_DIR}" ]; then
     done
     echo "Installing dependencies"
     # install python dependencies via pip
-    ${VSH_PIP_BIN} install -Iq -r ${VSH_REPO_DIR}/requirements.txt
+    ${VSH_PIP_BIN} install -q -r ${VSH_REPO_DIR}/requirements.txt
     # output log
     echo "Successfully installed version ${GIT_TAG}"
 else
